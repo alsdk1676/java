262d2c95909b1239ff8dab61b4d5453ae6bdc322
alsdk1676 <alsdk6761@gmail.com>
2025-02-05 21:54:54 +0900

    java day01 수정

--- variable/src/variableTest/VariableTask01.java
+++ variable/src/variableTest/VariableTask01.java
@@ -11,34 +11,33 @@ public class VariableTask01 {
 		
 //		알맞는 자료형에 담아보기
 //		5, false, 7845.15, B, 85.10F, 자바, S, 98590, D, true, .14, 공백문자
-		long num = 0;
+		long num = 0L;
 		int num1 = 5;
 		boolean isFalse = false;
-		double data1 = 7845.15;
-		char charB = 'B';
-		float data2 = 85.10F;
-		String java = "자바";
+		double money = 7845.15;
+		char grade = 'B';
+		float num3 = 85.10F;
+		String java = "JAVA";
 		char str1 = 'S';
-		int num2 = 98590;
-		char charD = 'D';
-//		String str2 = "D"
+		int num4 = 98590;
+		String str2 = "D";
 		boolean isTrue = true;
 		String str3 = ".14";
-		char empty = ' ';
+		char str4 = ' ';
 
 		System.out.println(num);
 		System.out.println(num1);
 		System.out.println(isFalse);
-		System.out.println(data1);
-		System.out.println(charB);
-		System.out.println(data2);
+		System.out.println(money);
+		System.out.println(grade);
+		System.out.println(num3);
 		System.out.println(java);
 		System.out.println(str1);
-		System.out.println(num2);
-		System.out.println(charD);
+		System.out.println(num4);
+		System.out.println(str2);
 		System.out.println(isTrue);
 		System.out.println(str3);
-		System.out.println(empty);
+		System.out.println(str4);
 	}
 
 }
