1b374f956b57c68f9276489f22d3faa659c14a2f
alsdk1676 <alsdk6761@gmail.com>
2025-02-11 01:26:10 +0900

    classTest review

--- classTest.zip_expanded/classTest/src/classTest/Company.java
+++ classTest.zip_expanded/classTest/src/classTest/Company.java
@@ -1,50 +1,62 @@
 package classTest;
 
+// 15분
 public class Company {
-	
-//	회사 클래스 필드 구성하기
-//	이름, 나이, 수입
+//  회사 클래스 필드 구성하기
+//  이름, 나이, 수입
 //	회사의 총 수입
-	static int money; // 회사마다 같은 값을 사용해야 되기 때문에
+	static int money;
 	String name;
 	int age;
 	int income;
-	
-//	기본 생성자
+		
 	public Company() {;}
 	
-//	생성자 (초기화)
 	public Company(String name, int age) {
 		this.name = name;
-		this.age = age; 
+		this.age = age;
 	}
 	
-	public static void main(String[] args) {
-//	회사원은 총 4명
+//	회사원은 총 4명(객체 생성)
 //	1번 +10000원
 //	2번 +1000원
 //	3번 +2000원
 //	4번 -20000원
 //	각 회사의 총 수입을 출력하기
+	public static void main(String[] args) {
 		Company employee1 = new Company("홍길동", 20);
 		Company employee2 = new Company("장보고", 30);
 		Company employee3 = new Company("이순신", 25);
-		Company employee4 = new Company("김유신", 35);
+		Company employee4 = new Company("김동건", 20);
 //		System.out.println(employee1.name);
 		
-		employee1.income += 10_000; // , 대신 _사용 (int)
+		employee1.income += 10_000;
 		employee2.income += 1_000;
 		employee3.income += 2_000;
-		employee4.income -= 2_0000;
+		employee4.income -= 20_000;
 		
 		Company.money += employee1.income;
 		Company.money += employee2.income;
 		Company.money += employee3.income;
 		Company.money += employee4.income;
 		
-		
 		System.out.println(Company.money);
+		
+		
 	}
-	
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- classTest2/src/classTest/StaticTest.java
+++ classTest2/src/classTest/StaticTest.java
@@ -20,7 +20,7 @@ public class StaticTest {
 //		st.increase();
 //		st.increase();
 //		st.increase();
-//		st = new StaticTest(); // 다른 주소값의 객체 ex)200번지
+//		st = new StaticTest(); // 다른 주소값의 객체(새로운 객체 생성) ex)200번지
 //		st.increase();
 //		st.increase();
 //		st.increase();
@@ -36,7 +36,7 @@ public class StaticTest {
 //		st.increase_s();
 //		st.increase_s();
 ////		heap 영역 , 영역이 달라서 값을 덮어쓸 수 없음!
-//		st = new StaticTest();
+//		st = new StaticTest(); // static은 객체와 상관없이 공유되기때문에 증가되고 있는 값이 유지됨
 //		st.increase_s();
 //		st.increase_s();
 //		st.increase_s();
@@ -58,6 +58,8 @@ public class StaticTest {
 		increase_s();
 		increase_s();
 		increase_s();
+		
+//		클래스 호출 없이도 변수에 접근 가능하다
 
 	}
 }
