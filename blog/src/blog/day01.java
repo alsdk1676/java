2539aac5259ae40d64bee870fb4c04fc9f41e115
alsdk1676 <alsdk6761@gmail.com>
2025-03-03 23:30:13 +0900

    forTask

---
+++ blog/src/blog/day01.java
@@ -0,0 +1,40 @@
+package blog;
+
+public class day01 {
+	
+	public static void main(String[] args) {
+		
+		
+		
+//		
+//		System.out.print("Hello");
+//		System.out.print(" World!");
+		
+		
+//		System.out.println("Hello");
+//		System.out.println("World!");
+		
+		
+		System.out.printf("이름 : %s, 나이 : %d세\n", "홍길동", 20);
+		
+		int x  = 10; // x는 4byte 크기의 공간에 10을 담고 있음.
+		
+		System.out.println(x);
+		
+		
+		final int a = 100;
+		
+		System.out.println(a);
+		
+		
+		String.valueOf(10); // 정수 10을 문자열 "10"으로 변환
+		
+		
+		
+		
+	}
+	
+	
+
+}
+
---
+++ forTask/src/forTask/forTask.java
@@ -0,0 +1,931 @@
+package forTask;
+
+public class forTask {
+	public static void main(String[] args) {
+		for(int i = 0; i < 10; i++) {
+			System.out.println(i);
+		}
+		
+		for(int i = 0; i < 10; i++) {
+			System.out.println(i + 1);
+		}
+		
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 == 0) {
+				System.out.println(i);
+			}
+		}
+		
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 != 0) {
+				System.out.println(i);
+			}
+		}
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 == 0) {
+				System.out.println(i);
+			}
+		}
+		
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 != 0) {
+				System.out.println(i);
+			}
+		}
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 == 0) {
+				System.out.println(i);
+			}
+		}
+		
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 != 0) {
+				System.out.println(i);
+			}
+		}
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 == 0) {
+				System.out.println(i);
+			}
+		}
+		
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 != 0) {
+				System.out.println(i);
+			}
+		}
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 == 0) {
+				System.out.println(i);
+			}
+		}
+		
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 != 0) {
+				System.out.println(i);
+			}
+		}
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 == 0) {
+				System.out.println(i);
+			}
+		}
+		
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 != 0) {
+				System.out.println(i);
+			}
+		}
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 == 0) {
+				System.out.println(i);
+			}
+		}
+		
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 != 0) {
+				System.out.println(i);
+			}
+		}
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 == 0) {
+				System.out.println(i);
+			}
+		}
+		
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 != 0) {
+				System.out.println(i);
+			}
+		}
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 == 0) {
+				System.out.println(i);
+			}
+		}
+		
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 != 0) {
+				System.out.println(i);
+			}
+		}
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 == 0) {
+				System.out.println(i);
+			}
+		}
+		
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 != 0) {
+				System.out.println(i);
+			}
+		}
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 == 0) {
+				System.out.println(i);
+			}
+		}
+		
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 != 0) {
+				System.out.println(i);
+			}
+		}
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 == 0) {
+				System.out.println(i);
+			}
+		}
+		
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 != 0) {
+				System.out.println(i);
+			}
+		}
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 == 0) {
+				System.out.println(i);
+			}
+		}
+		
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 != 0) {
+				System.out.println(i);
+			}
+		}
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 == 0) {
+				System.out.println(i);
+			}
+		}
+		
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 != 0) {
+				System.out.println(i);
+			}
+		}
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 == 0) {
+				System.out.println(i);
+			}
+		}
+		
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 != 0) {
+				System.out.println(i);
+			}
+		}
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 == 0) {
+				System.out.println(i);
+			}
+		}
+		
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 != 0) {
+				System.out.println(i);
+			}
+		}
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 == 0) {
+				System.out.println(i);
+			}
+		}
+		
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 != 0) {
+				System.out.println(i);
+			}
+		}
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 == 0) {
+				System.out.println(i);
+			}
+		}
+		
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 != 0) {
+				System.out.println(i);
+			}
+		}
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 == 0) {
+				System.out.println(i);
+			}
+		}
+		
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 != 0) {
+				System.out.println(i);
+			}
+		}
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 == 0) {
+				System.out.println(i);
+			}
+		}
+		
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 != 0) {
+				System.out.println(i);
+			}
+		}
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 == 0) {
+				System.out.println(i);
+			}
+		}
+		
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 != 0) {
+				System.out.println(i);
+			}
+		}
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 == 0) {
+				System.out.println(i);
+			}
+		}
+		
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 != 0) {
+				System.out.println(i);
+			}
+		}
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 == 0) {
+				System.out.println(i);
+			}
+		}
+		
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 != 0) {
+				System.out.println(i);
+			}
+		}
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 == 0) {
+				System.out.println(i);
+			}
+		}
+		
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 != 0) {
+				System.out.println(i);
+			}
+		}
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 == 0) {
+				System.out.println(i);
+			}
+		}
+		
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 != 0) {
+				System.out.println(i);
+			}
+		}
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 == 0) {
+				System.out.println(i);
+			}
+		}
+		
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 != 0) {
+				System.out.println(i);
+			}
+		}
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 == 0) {
+				System.out.println(i);
+			}
+		}
+		
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 != 0) {
+				System.out.println(i);
+			}
+		}
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 == 0) {
+				System.out.println(i);
+			}
+		}
+		
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 != 0) {
+				System.out.println(i);
+			}
+		}
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 == 0) {
+				System.out.println(i);
+			}
+		}
+		
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 != 0) {
+				System.out.println(i);
+			}
+		}
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 == 0) {
+				System.out.println(i);
+			}
+		}
+		
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 != 0) {
+				System.out.println(i);
+			}
+		}
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 == 0) {
+				System.out.println(i);
+			}
+		}
+		
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 != 0) {
+				System.out.println(i);
+			}
+		}
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 == 0) {
+				System.out.println(i);
+			}
+		}
+		
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 != 0) {
+				System.out.println(i);
+			}
+		}
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 == 0) {
+				System.out.println(i);
+			}
+		}
+		
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 != 0) {
+				System.out.println(i);
+			}
+		}
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 == 0) {
+				System.out.println(i);
+			}
+		}
+		
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 != 0) {
+				System.out.println(i);
+			}
+		}
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 == 0) {
+				System.out.println(i);
+			}
+		}
+		
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 != 0) {
+				System.out.println(i);
+			}
+		}
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 == 0) {
+				System.out.println(i);
+			}
+		}
+		
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 != 0) {
+				System.out.println(i);
+			}
+		}
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 == 0) {
+				System.out.println(i);
+			}
+		}
+		
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 != 0) {
+				System.out.println(i);
+			}
+		}
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 == 0) {
+				System.out.println(i);
+			}
+		}
+		
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 != 0) {
+				System.out.println(i);
+			}
+		}
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 == 0) {
+				System.out.println(i);
+			}
+		}
+		
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 != 0) {
+				System.out.println(i);
+			}
+		}
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 == 0) {
+				System.out.println(i);
+			}
+		}
+		
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 != 0) {
+				System.out.println(i);
+			}
+		}
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 == 0) {
+				System.out.println(i);
+			}
+		}
+		
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 != 0) {
+				System.out.println(i);
+			}
+		}
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 == 0) {
+				System.out.println(i);
+			}
+		}
+		
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 != 0) {
+				System.out.println(i);
+			}
+		}
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 == 0) {
+				System.out.println(i);
+			}
+		}
+		
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 != 0) {
+				System.out.println(i);
+			}
+		}
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 == 0) {
+				System.out.println(i);
+			}
+		}
+		
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 != 0) {
+				System.out.println(i);
+			}
+		}
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 == 0) {
+				System.out.println(i);
+			}
+		}
+		
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 != 0) {
+				System.out.println(i);
+			}
+		}
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 == 0) {
+				System.out.println(i);
+			}
+		}
+		
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 != 0) {
+				System.out.println(i);
+			}
+		}
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 == 0) {
+				System.out.println(i);
+			}
+		}
+		
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 != 0) {
+				System.out.println(i);
+			}
+		}
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 == 0) {
+				System.out.println(i);
+			}
+		}
+		
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 != 0) {
+				System.out.println(i);
+			}
+		}
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 == 0) {
+				System.out.println(i);
+			}
+		}
+		
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 != 0) {
+				System.out.println(i);
+			}
+		}
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 == 0) {
+				System.out.println(i);
+			}
+		}
+		
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 != 0) {
+				System.out.println(i);
+			}
+		}
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 == 0) {
+				System.out.println(i);
+			}
+		}
+		
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 != 0) {
+				System.out.println(i);
+			}
+		}
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 == 0) {
+				System.out.println(i);
+			}
+		}
+		
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 != 0) {
+				System.out.println(i);
+			}
+		}
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 == 0) {
+				System.out.println(i);
+			}
+		}
+		
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 != 0) {
+				System.out.println(i);
+			}
+		}
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 == 0) {
+				System.out.println(i);
+			}
+		}
+		
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 != 0) {
+				System.out.println(i);
+			}
+		}
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 == 0) {
+				System.out.println(i);
+			}
+		}
+		
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 != 0) {
+				System.out.println(i);
+			}
+		}
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 == 0) {
+				System.out.println(i);
+			}
+		}
+		
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 != 0) {
+				System.out.println(i);
+			}
+		}
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 == 0) {
+				System.out.println(i);
+			}
+		}
+		
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 != 0) {
+				System.out.println(i);
+			}
+		}
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 == 0) {
+				System.out.println(i);
+			}
+		}
+		
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 != 0) {
+				System.out.println(i);
+			}
+		}
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 == 0) {
+				System.out.println(i);
+			}
+		}
+		
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 != 0) {
+				System.out.println(i);
+			}
+		}
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 == 0) {
+				System.out.println(i);
+			}
+		}
+		
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 != 0) {
+				System.out.println(i);
+			}
+		}
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 == 0) {
+				System.out.println(i);
+			}
+		}
+		
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 != 0) {
+				System.out.println(i);
+			}
+		}
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 == 0) {
+				System.out.println(i);
+			}
+		}
+		
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 != 0) {
+				System.out.println(i);
+			}
+		}
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 == 0) {
+				System.out.println(i);
+			}
+		}
+		
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 != 0) {
+				System.out.println(i);
+			}
+		}
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 == 0) {
+				System.out.println(i);
+			}
+		}
+		
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 != 0) {
+				System.out.println(i);
+			}
+		}
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 == 0) {
+				System.out.println(i);
+			}
+		}
+		
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 != 0) {
+				System.out.println(i);
+			}
+		}
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 == 0) {
+				System.out.println(i);
+			}
+		}
+		
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 != 0) {
+				System.out.println(i);
+			}
+		}
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 == 0) {
+				System.out.println(i);
+			}
+		}
+		
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 != 0) {
+				System.out.println(i);
+			}
+		}
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 == 0) {
+				System.out.println(i);
+			}
+		}
+		
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 != 0) {
+				System.out.println(i);
+			}
+		}
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 == 0) {
+				System.out.println(i);
+			}
+		}
+		
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 != 0) {
+				System.out.println(i);
+			}
+		}
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 == 0) {
+				System.out.println(i);
+			}
+		}
+		
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 != 0) {
+				System.out.println(i);
+			}
+		}
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 == 0) {
+				System.out.println(i);
+			}
+		}
+		
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 != 0) {
+				System.out.println(i);
+			}
+		}
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 == 0) {
+				System.out.println(i);
+			}
+		}
+		
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 != 0) {
+				System.out.println(i);
+			}
+		}
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 == 0) {
+				System.out.println(i);
+			}
+		}
+		
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 != 0) {
+				System.out.println(i);
+			}
+		}
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 == 0) {
+				System.out.println(i);
+			}
+		}
+		
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 != 0) {
+				System.out.println(i);
+			}
+		}
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 == 0) {
+				System.out.println(i);
+			}
+		}
+		
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 != 0) {
+				System.out.println(i);
+			}
+		}
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 == 0) {
+				System.out.println(i);
+			}
+		}
+		
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 != 0) {
+				System.out.println(i);
+			}
+		}
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 == 0) {
+				System.out.println(i);
+			}
+		}
+		
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 != 0) {
+				System.out.println(i);
+			}
+		}
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 == 0) {
+				System.out.println(i);
+			}
+		}
+		
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 != 0) {
+				System.out.println(i);
+			}
+		}
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 == 0) {
+				System.out.println(i);
+			}
+		}
+		
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 != 0) {
+				System.out.println(i);
+			}
+		}
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 == 0) {
+				System.out.println(i);
+			}
+		}
+		
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 != 0) {
+				System.out.println(i);
+			}
+		}
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 == 0) {
+				System.out.println(i);
+			}
+		}
+		
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 != 0) {
+				System.out.println(i);
+			}
+		}
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 == 0) {
+				System.out.println(i);
+			}
+		}
+		
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 != 0) {
+				System.out.println(i);
+			}
+		}
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 == 0) {
+				System.out.println(i);
+			}
+		}
+		
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 != 0) {
+				System.out.println(i);
+			}
+		}
+		
+
+		
+	}
+
+}
