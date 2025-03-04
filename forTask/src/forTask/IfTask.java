6e4b23557c6103ff36e2436d4e0dd065ae677697
alsdk1676 <alsdk6761@gmail.com>
2025-03-04 23:57:59 +0900

    IfTask02

--- forTask/src/forTask/IfTask.java
+++ forTask/src/forTask/IfTask.java
@@ -11,16 +11,16 @@ public class IfTask {
 //		100 미만이면 "100 미만입니다."를 출력하세요.
 		
 		Scanner sc = new Scanner(System.in);
-//		int input = 0;
-//		String message = "정수를 입력하세요.";
-//		System.out.println(message);
-//		input = sc.nextInt();
-//		
-//		if(input >= 100) {
-//			System.out.println("100 이상입니다.");
-//		} else {
-//			System.out.println("100 미만입니다.");
-//		}
+		int input = 0;
+		String message = "정수를 입력하세요.";
+		System.out.println(message);
+		input = sc.nextInt();
+		
+		if(input >= 100) {
+			System.out.println("100 이상입니다.");
+		} else {
+			System.out.println("100 미만입니다.");
+		}
 	
 	
 //		문제 2:
@@ -28,18 +28,18 @@ public class IfTask {
 //		나이가 18세 이상이면 "성인 영화 관람 가능"을 출력하고,
 //		나이가 13세 이상 18세 미만이면 "청소년 영화 관람 가능"을 출력하고,
 //		나이가 13세 미만이면 "어린이 영화 관람 가능"을 출력하세요.
-//		int age = 0;
-//		String message2 = "나이를 입력하세요.";
-//		System.out.println(message2);
-//		age = sc.nextInt();
-//		
-//		if(age >= 18) {
-//			System.out.println("성인 영화 관람 가능");
-//		} else if (age >= 13) {
-//			System.out.println("청소년 영화 관람 가능");
-//		} else {
-//			System.out.println("어린이 영화 관람 가능");
-//		}
+		int age = 0;
+		String message2 = "나이를 입력하세요.";
+		System.out.println(message2);
+		age = sc.nextInt();
+		
+		if(age >= 18) {
+			System.out.println("성인 영화 관람 가능");
+		} else if (age >= 13) {
+			System.out.println("청소년 영화 관람 가능");
+		} else {
+			System.out.println("어린이 영화 관람 가능");
+		}
 		
 //		문제 3:
 //		사용자가 입력한 성적을 바탕으로 학점을 출력하는 프로그램을 작성해 보세요.
@@ -64,17 +64,30 @@ public class IfTask {
 		} else {
 			System.out.println("F");
 		}
-		
+//		
 //		문제 4:
 //		사용자가 입력한 두 숫자 중 더 큰 값을 출력하는 프로그램을 작성해 보세요.
 //		두 숫자 중 더 큰 숫자를 출력하세요.
 		
 //		문제 5:
 //		사용자가 입력한 숫자가 3의 배수인지 5의 배수인지를 판별하는 프로그램을 작성해 보세요.
-//
+
 //		3의 배수이면 "3의 배수입니다."를 출력하고,
 //		5의 배수이면 "5의 배수입니다."를 출력하고,
 //		3의 배수도 5의 배수도 아니면 "3의 배수도, 5의 배수도 아닙니다."를 출력하세요.
+		int inputNum = 0;
+		String message5 = "정수를 입력하세요.";
+		System.out.println(message5);
+		inputNum = sc.nextInt();
+		
+		if(inputNum % 3 == 0) {
+			System.out.println("3의 배수입니다.");
+		} else if(inputNum % 5 == 0) {
+			System.out.println("5의 배수입니다.");
+		} else {
+			System.out.println("3의 배수도, 5의 배수도 아닙니다.");
+		}
+	
 	
 
 	}
