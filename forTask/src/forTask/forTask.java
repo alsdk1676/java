295a76eb5dd2baf5d708dd618c2ba4da2cd9c767
alsdk1676 <alsdk6761@gmail.com>
2025-03-08 23:08:13 +0900

    forTask

--- forTask/src/forTask/forTask.java
+++ forTask/src/forTask/forTask.java
@@ -1016,6 +1016,270 @@ public class forTask {
 			System.out.println(i + 1 + "번 째 서민아"); // i + 1 괄호 필요 X (코드는 좌에서 우로)
 		}
 		
+		
+		for(int i = 0; i < 10; i++) {
+			System.out.println(i + 1 + "번 째 서민아"); // i + 1 괄호 필요 X (코드는 좌에서 우로)
+		}		for(int i = 0; i < 10; i++) { // i의 마지막 값 : 9가 아니라 10!!!
+			System.out.println("서민아");
+		}
+		
+		for(int i = 0; i < 10; i++) {
+			System.out.println(i + 1 + "번 째 서민아"); // i + 1 괄호 필요 X (코드는 좌에서 우로)
+		}
+		
+		
+		for(int i = 0; i < 10; i++) {
+			System.out.println(i + 1 + "번 째 서민아"); // i + 1 괄호 필요 X (코드는 좌에서 우로)
+		}		for(int i = 0; i < 10; i++) { // i의 마지막 값 : 9가 아니라 10!!!
+			System.out.println("서민아");
+		}
+		
+		for(int i = 0; i < 10; i++) {
+			System.out.println(i + 1 + "번 째 서민아"); // i + 1 괄호 필요 X (코드는 좌에서 우로)
+		}
+		
+		
+		for(int i = 0; i < 10; i++) {
+			System.out.println(i + 1 + "번 째 서민아"); // i + 1 괄호 필요 X (코드는 좌에서 우로)
+		}		for(int i = 0; i < 10; i++) { // i의 마지막 값 : 9가 아니라 10!!!
+			System.out.println("서민아");
+		}
+		
+		for(int i = 0; i < 10; i++) {
+			System.out.println(i + 1 + "번 째 서민아"); // i + 1 괄호 필요 X (코드는 좌에서 우로)
+		}
+		
+		
+		for(int i = 0; i < 10; i++) {
+			System.out.println(i + 1 + "번 째 서민아"); // i + 1 괄호 필요 X (코드는 좌에서 우로)
+		}		for(int i = 0; i < 10; i++) { // i의 마지막 값 : 9가 아니라 10!!!
+			System.out.println("서민아");
+		}
+		
+		for(int i = 0; i < 10; i++) {
+			System.out.println(i + 1 + "번 째 서민아"); // i + 1 괄호 필요 X (코드는 좌에서 우로)
+		}
+		
+		
+		for(int i = 0; i < 10; i++) {
+			System.out.println(i + 1 + "번 째 서민아"); // i + 1 괄호 필요 X (코드는 좌에서 우로)
+		}		for(int i = 0; i < 10; i++) { // i의 마지막 값 : 9가 아니라 10!!!
+			System.out.println("서민아");
+		}
+		
+		for(int i = 0; i < 10; i++) {
+			System.out.println(i + 1 + "번 째 서민아"); // i + 1 괄호 필요 X (코드는 좌에서 우로)
+		}
+		
+		
+		for(int i = 0; i < 10; i++) {
+			System.out.println(i + 1 + "번 째 서민아"); // i + 1 괄호 필요 X (코드는 좌에서 우로)
+		}		for(int i = 0; i < 10; i++) { // i의 마지막 값 : 9가 아니라 10!!!
+			System.out.println("서민아");
+		}
+		
+		for(int i = 0; i < 10; i++) {
+			System.out.println(i + 1 + "번 째 서민아"); // i + 1 괄호 필요 X (코드는 좌에서 우로)
+		}
+		
+		
+		for(int i = 0; i < 10; i++) {
+			System.out.println(i + 1 + "번 째 서민아"); // i + 1 괄호 필요 X (코드는 좌에서 우로)
+		}		for(int i = 0; i < 10; i++) { // i의 마지막 값 : 9가 아니라 10!!!
+			System.out.println("서민아");
+		}
+		
+		for(int i = 0; i < 10; i++) {
+			System.out.println(i + 1 + "번 째 서민아"); // i + 1 괄호 필요 X (코드는 좌에서 우로)
+		}
+		
+		
+		for(int i = 0; i < 10; i++) {
+			System.out.println(i + 1 + "번 째 서민아"); // i + 1 괄호 필요 X (코드는 좌에서 우로)
+		}		for(int i = 0; i < 10; i++) { // i의 마지막 값 : 9가 아니라 10!!!
+			System.out.println("서민아");
+		}
+		
+		for(int i = 0; i < 10; i++) {
+			System.out.println(i + 1 + "번 째 서민아"); // i + 1 괄호 필요 X (코드는 좌에서 우로)
+		}
+		
+		
+		for(int i = 0; i < 10; i++) {
+			System.out.println(i + 1 + "번 째 서민아"); // i + 1 괄호 필요 X (코드는 좌에서 우로)
+		}		for(int i = 0; i < 10; i++) { // i의 마지막 값 : 9가 아니라 10!!!
+			System.out.println("서민아");
+		}
+		
+		for(int i = 0; i < 10; i++) {
+			System.out.println(i + 1 + "번 째 서민아"); // i + 1 괄호 필요 X (코드는 좌에서 우로)
+		}
+		
+		
+		for(int i = 0; i < 10; i++) {
+			System.out.println(i + 1 + "번 째 서민아"); // i + 1 괄호 필요 X (코드는 좌에서 우로)
+		}		for(int i = 0; i < 10; i++) { // i의 마지막 값 : 9가 아니라 10!!!
+			System.out.println("서민아");
+		}
+		
+		for(int i = 0; i < 10; i++) {
+			System.out.println(i + 1 + "번 째 서민아"); // i + 1 괄호 필요 X (코드는 좌에서 우로)
+		}
+		
+		
+		for(int i = 0; i < 10; i++) {
+			System.out.println(i + 1 + "번 째 서민아"); // i + 1 괄호 필요 X (코드는 좌에서 우로)
+		}		for(int i = 0; i < 10; i++) { // i의 마지막 값 : 9가 아니라 10!!!
+			System.out.println("서민아");
+		}
+		
+		for(int i = 0; i < 10; i++) {
+			System.out.println(i + 1 + "번 째 서민아"); // i + 1 괄호 필요 X (코드는 좌에서 우로)
+		}
+		
+		
+		for(int i = 0; i < 10; i++) {
+			System.out.println(i + 1 + "번 째 서민아"); // i + 1 괄호 필요 X (코드는 좌에서 우로)
+		}		for(int i = 0; i < 10; i++) { // i의 마지막 값 : 9가 아니라 10!!!
+			System.out.println("서민아");
+		}
+		
+		for(int i = 0; i < 10; i++) {
+			System.out.println(i + 1 + "번 째 서민아"); // i + 1 괄호 필요 X (코드는 좌에서 우로)
+		}
+		
+		
+		for(int i = 0; i < 10; i++) {
+			System.out.println(i + 1 + "번 째 서민아"); // i + 1 괄호 필요 X (코드는 좌에서 우로)
+		}		for(int i = 0; i < 10; i++) { // i의 마지막 값 : 9가 아니라 10!!!
+			System.out.println("서민아");
+		}
+		
+		for(int i = 0; i < 10; i++) {
+			System.out.println(i + 1 + "번 째 서민아"); // i + 1 괄호 필요 X (코드는 좌에서 우로)
+		}
+		
+		
+		for(int i = 0; i < 10; i++) {
+			System.out.println(i + 1 + "번 째 서민아"); // i + 1 괄호 필요 X (코드는 좌에서 우로)
+		}		for(int i = 0; i < 10; i++) { // i의 마지막 값 : 9가 아니라 10!!!
+			System.out.println("서민아");
+		}
+		
+		for(int i = 0; i < 10; i++) {
+			System.out.println(i + 1 + "번 째 서민아"); // i + 1 괄호 필요 X (코드는 좌에서 우로)
+		}
+		
+		
+		for(int i = 0; i < 10; i++) {
+			System.out.println(i + 1 + "번 째 서민아"); // i + 1 괄호 필요 X (코드는 좌에서 우로)
+		}		for(int i = 0; i < 10; i++) { // i의 마지막 값 : 9가 아니라 10!!!
+			System.out.println("서민아");
+		}
+		
+		for(int i = 0; i < 10; i++) {
+			System.out.println(i + 1 + "번 째 서민아"); // i + 1 괄호 필요 X (코드는 좌에서 우로)
+		}
+		
+		
+		for(int i = 0; i < 10; i++) {
+			System.out.println(i + 1 + "번 째 서민아"); // i + 1 괄호 필요 X (코드는 좌에서 우로)
+		}		for(int i = 0; i < 10; i++) { // i의 마지막 값 : 9가 아니라 10!!!
+			System.out.println("서민아");
+		}
+		
+		for(int i = 0; i < 10; i++) {
+			System.out.println(i + 1 + "번 째 서민아"); // i + 1 괄호 필요 X (코드는 좌에서 우로)
+		}
+		
+		
+		for(int i = 0; i < 10; i++) {
+			System.out.println(i + 1 + "번 째 서민아"); // i + 1 괄호 필요 X (코드는 좌에서 우로)
+		}		for(int i = 0; i < 10; i++) { // i의 마지막 값 : 9가 아니라 10!!!
+			System.out.println("서민아");
+		}
+		
+		for(int i = 0; i < 10; i++) {
+			System.out.println(i + 1 + "번 째 서민아"); // i + 1 괄호 필요 X (코드는 좌에서 우로)
+		}
+		
+		
+		for(int i = 0; i < 10; i++) {
+			System.out.println(i + 1 + "번 째 서민아"); // i + 1 괄호 필요 X (코드는 좌에서 우로)
+		}		for(int i = 0; i < 10; i++) { // i의 마지막 값 : 9가 아니라 10!!!
+			System.out.println("서민아");
+		}
+		
+		for(int i = 0; i < 10; i++) {
+			System.out.println(i + 1 + "번 째 서민아"); // i + 1 괄호 필요 X (코드는 좌에서 우로)
+		}
+		
+		
+		for(int i = 0; i < 10; i++) {
+			System.out.println(i + 1 + "번 째 서민아"); // i + 1 괄호 필요 X (코드는 좌에서 우로)
+		}		for(int i = 0; i < 10; i++) { // i의 마지막 값 : 9가 아니라 10!!!
+			System.out.println("서민아");
+		}
+		
+		for(int i = 0; i < 10; i++) {
+			System.out.println(i + 1 + "번 째 서민아"); // i + 1 괄호 필요 X (코드는 좌에서 우로)
+		}
+		
+		
+		for(int i = 0; i < 10; i++) {
+			System.out.println(i + 1 + "번 째 서민아"); // i + 1 괄호 필요 X (코드는 좌에서 우로)
+		}		for(int i = 0; i < 10; i++) { // i의 마지막 값 : 9가 아니라 10!!!
+			System.out.println("서민아");
+		}
+		
+		for(int i = 0; i < 10; i++) {
+			System.out.println(i + 1 + "번 째 서민아"); // i + 1 괄호 필요 X (코드는 좌에서 우로)
+		}
+		
+		
+		for(int i = 0; i < 10; i++) {
+			System.out.println(i + 1 + "번 째 서민아"); // i + 1 괄호 필요 X (코드는 좌에서 우로)
+		}		for(int i = 0; i < 10; i++) { // i의 마지막 값 : 9가 아니라 10!!!
+			System.out.println("서민아");
+		}
+		
+		for(int i = 0; i < 10; i++) {
+			System.out.println(i + 1 + "번 째 서민아"); // i + 1 괄호 필요 X (코드는 좌에서 우로)
+		}
+		
+		
+		for(int i = 0; i < 10; i++) {
+			System.out.println(i + 1 + "번 째 서민아"); // i + 1 괄호 필요 X (코드는 좌에서 우로)
+		}		for(int i = 0; i < 10; i++) { // i의 마지막 값 : 9가 아니라 10!!!
+			System.out.println("서민아");
+		}
+		
+		for(int i = 0; i < 10; i++) {
+			System.out.println(i + 1 + "번 째 서민아"); // i + 1 괄호 필요 X (코드는 좌에서 우로)
+		}
+		
+		
+		for(int i = 0; i < 10; i++) {
+			System.out.println(i + 1 + "번 째 서민아"); // i + 1 괄호 필요 X (코드는 좌에서 우로)
+		}		for(int i = 0; i < 10; i++) { // i의 마지막 값 : 9가 아니라 10!!!
+			System.out.println("서민아");
+		}
+		
+		for(int i = 0; i < 10; i++) {
+			System.out.println(i + 1 + "번 째 서민아"); // i + 1 괄호 필요 X (코드는 좌에서 우로)
+		}
+		
+		
+		for(int i = 0; i < 10; i++) {
+			System.out.println(i + 1 + "번 째 서민아"); // i + 1 괄호 필요 X (코드는 좌에서 우로)
+		}		for(int i = 0; i < 10; i++) { // i의 마지막 값 : 9가 아니라 10!!!
+			System.out.println("서민아");
+		}
+		
+		for(int i = 0; i < 10; i++) {
+			System.out.println(i + 1 + "번 째 서민아"); // i + 1 괄호 필요 X (코드는 좌에서 우로)
+		}
+		
 
 		
 	}
