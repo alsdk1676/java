a9ce6698600c695993453957f6640ce0fa0913f6
alsdk1676 <alsdk6761@gmail.com>
2025-06-15 23:53:05 +0900

    casting 0615

--- castingTest/src/castingTask3/CastingTask.java
+++ castingTest/src/castingTask3/CastingTask.java
@@ -3,9 +3,10 @@ package castingTask3;
 public class CastingTask {
 	public void orderFood(Food food) {
 		if(food instanceof Burger) {
+//			다운 캐스팅 (부모 -> 자식)
 			Burger burger = (Burger)food;
 			burger.addKetchup();
-		} else if(food instanceof Pizza) {
+		}else if(food instanceof Pizza) {
 			Pizza pizza = (Pizza)food;
 			pizza.addCheese();
 		}else if(food instanceof Sushi) {
@@ -15,6 +16,8 @@ public class CastingTask {
 	}
 	public static void main(String[] args) {
 		CastingTask ct = new CastingTask();
+		// Food food = new Burger();
+		// 업캐스팅 : 자식 -> 부모 (명시 안해줘도 자동으로)
 		ct.orderFood(new Burger());
 		ct.orderFood(new Pizza());
 		ct.orderFood(new Sushi());
